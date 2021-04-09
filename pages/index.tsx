74aacf57483656cb2c39c8f454e95c7b852bc707
Harry <gelo.soer@gmail.com>
2021-04-10 03:17:48 +0800

    added login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,60 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+import { Box, Button, FormControl, Input, Text } from '@chakra-ui/react'
 
-export default function Home() {
+const Home = () => {
+  const router = useRouter()
+
+  const onSubmit = () => {
+    router.push('/admin')
+  }
+
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Box
+      minHeight="100vh"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        shadow="xl"
+        padding="30px"
+        border="1px"
+        borderRadius="xl"
+        borderColor="gray.200"
+        minWidth="400px"
+      >
+        <Text
+          fontSize="2xl"
+          textAlign="center"
+          fontWeight="bold"
+          marginBottom="9"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+          Login
+        </Text>
+
+        <form onSubmit={onSubmit}>
+          <FormControl>
+            <Input placeholder="Email" marginBottom="15px" />
+            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+          </FormControl>
+          <FormControl>
+            <Input placeholder="Password" type="password" />
+            {/* <FormErrorMessage>{form.errors.name}</FormErrorMessage> */}
+          </FormControl>
+
+          <Button
+            type="submit"
+            colorScheme="blue"
+            display="block"
+            marginLeft="auto"
+            marginTop="20px"
+          >
+            Sign-in
+          </Button>
+        </form>
+      </Box>
+    </Box>
   )
 }
+
+export default Home
