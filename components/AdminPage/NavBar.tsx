716605dd779280ea410673e7953d41dfa83492e1
Harry <gelo.soer@gmail.com>
2021-04-10 03:22:51 +0800

    setup for admin content

---
+++ components/AdminPage/NavBar.tsx
@@ -0,0 +1,15 @@
+import { Box, Text } from "@chakra-ui/layout"
+
+const NavBar = () => {
+  return (
+    <Box
+      width="100vw"
+      paddingY="30px"
+      paddingX="20px"
+    >
+      <Text>Admin</Text>
+    </Box>
+  )
+}
+
+export default NavBar
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,8 +1,14 @@
+import { Box } from "@chakra-ui/layout"
+import NavBar from "components/AdminPage/NavBar"
+
 const Admin = () => {
   return (
-    <div>
-      Admin page
-    </div>
+    <>
+      <NavBar />
+      <Box paddingTop="30px" paddingX="20px">
+        Content
+      </Box>
+    </>
   )
 }
 
