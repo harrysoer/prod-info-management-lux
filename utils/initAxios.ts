974f7b6dd71bf3eff3212ae83e679aa1d493c881
Harry <gelo.soer@gmail.com>
2021-04-12 10:00:10 +0800

    added CORS override on axios init

--- utils/initAxios.ts
+++ utils/initAxios.ts
@@ -1,10 +1,11 @@
 import Axios from "axios";
 import { configure } from "axios-hooks";
 
-const initAxios = (token: string) => {
+const initAxios = (token: string = "") => {
   const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
+      "Access-Control-Allow-Origin": "*",
       authorization: token,
     },
   });
